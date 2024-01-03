389004a842141cf0133f6db0e6ca15bff3224806
bedrock-adam <adammikulas@gmail.com>
2024-01-04 09:01:40 +1100

    update outboxer publisher

--- generators/outboxer/templates/bin/outboxer_publisher.rb
+++ generators/outboxer/templates/bin/outboxer_publisher.rb
@@ -3,7 +3,6 @@
 require 'bundler/setup'
 require 'logger'
 require 'sidekiq'
-require 'pry-byebug'
 
 require_relative '../../../../lib/outboxer'
 
@@ -13,7 +12,9 @@ logger.level = Logger::INFO
 options = Outboxer::OptionParser.parse!
 
 Sidekiq.configure_client { |config| config.redis = { url: options[:redis_url] } }
-options[:handlers].values.each { |worker_path| require_relative worker_path }
+
+require_relative 'workers/domain_event_handler_worker'
+require_relative 'workers/log_handler_worker'
 
 outboxer_publisher = Outboxer::Publisher.new(
   db_config: options[:db_config], poll: options[:poll], logger: logger)
@@ -21,13 +22,17 @@ outboxer_publisher = Outboxer::Publisher.new(
 trap("INT") { outboxer_publisher.stop! }
 
 outboxer_publisher.publish! do |outboxer_message|
-  handler = options[:handlers][outboxer_message.outboxer_messageable_type]
-  handler.constantize.perform_async({ 'id' => outboxer_message.outboxer_messageable_id })
+  case outboxer_message.outboxer_messageable_type
+  when 'Models::DomainEvent'
+    DomainEventHandlerWorker.perform_async({ 'id' => outboxer_message.outboxer_messageable_id })
+  when 'Models::Log'
+    LogHandlerWorker.perform_async({ 'id' => outboxer_message.outboxer_messageable_id })
+  else
+    raise "No handler defined for #{outboxer_message.outboxer_messageable_type}"
+  end
 end
 
 # bin/outboxer_publisher \
 #   --db_config=spec/config/database.yml
 #   --poll=1
 #   --redis_url=redis://localhost:6379/0
-#   --messageable_type=Models::DomainEvent --worker=workers/domain_event_handler_worker.rb \
-#   --messageable_type=Models::Log         --worker=workers/log_handler_worker.rb
