d9f7d8684ac8cf43ec09a56647fe83eaf5312df7
bedrock-adam <adammikulas@gmail.com>
2024-01-14 14:59:59 +1100

    encapsulate synchronous non blocking code

--- lib/outboxer/publisher.rb
+++ lib/outboxer/publisher.rb
@@ -2,110 +2,119 @@ module Outboxer
   module Publisher
     extend self
 
-    def connect!(db_config:, logger: nil)
-      ActiveRecord::Base.establish_connection(db_config)
+    class Error < StandardError; end
 
+    def connect!(db_config:, logger: nil)
       ActiveRecord::Base.logger = logger if logger
+
+      ActiveRecord::Base.establish_connection(db_config)
     end
 
     def stop!
       @running = false
     end
 
-    def pop_messages_async!(&block)
-      @threads_max.times { @threads << pop_message_async!(&block) }
-    end
+    class ThreadShutdown < Error; end
 
-    def pop_message_async!(&block)
-      Thread.new do
-        loop do
-          outboxer_message = @queue.pop
-          break if outboxer_message.nil?
+    def pop_message!(queue:, logger:, &block)
+      outboxer_message = queue.pop
+      raise ThreadShutdown if outboxer_message.nil?
 
-          @logger.debug "Processing message id: #{outboxer_message.id}"
+      logger.debug "Processing message id: #{outboxer_message.id}"
 
-          begin
-            block.call(outboxer_message)
-          rescue => exception
-            @logger.error "Message processing failed for id: #{outboxer_message.id}, error: #{exception}"
+      begin
+        block.call(outboxer_message)
+      rescue => exception
+        logger.error "Message processing failed for id: #{outboxer_message.id}, error: #{exception}"
 
-            Message.failed!(id: outboxer_message.id, exception: exception)
+        Message.failed!(id: outboxer_message.id, exception: exception)
 
-            raise
-          end
-
-          Message.published!(id: outboxer_message.id)
-
-          @logger.debug "Message processed successfully for id: #{outboxer_message.id}"
-        rescue => exception
-          @logger.error "Error: #{exception.class}: #{exception.message}"
-        rescue Exception => exception
-          @logger.fatal "Critical Error: #{exception.class}: #{exception.message}"
-
-          stop!
-
-          break
-        end
+        raise
       end
+
+      Message.published!(id: outboxer_message.id)
+
+      logger.debug "Message processed successfully for id: #{outboxer_message.id}"
+    rescue ThreadShutdown
+      raise
+    rescue => exception
+      logger.error "Error: #{exception.class}: #{exception.message}"
     end
 
-    def push_messages!
+    def push_messages!(threads:, queue:, queue_max:, poll:, logger:)
       @running = true
 
       while @running
         messages = []
 
-        queue_remaining = @queue_max - @queue.length
+        queue_remaining = queue_max - queue.length
         messages = (queue_remaining > 0) ? Outboxer::Message.unpublished!(limit: queue_remaining) : []
 
         if messages.empty?
-          log("Sleeping for #{@poll} seconds because there are no messages", messages: messages)
-          sleep(@poll)
-          log("Slept for #{@poll} seconds", messages: messages)
+          log("Sleeping for #{poll} seconds because there are no messages",
+            logger: logger, threads: threads, queue: queue, messages: messages)
+
+          sleep(poll)
+
+          log("Slept for #{poll} seconds",
+            logger: logger, threads: threads, queue: queue, messages: messages)
 
           next
         end
 
-        log("Pushing #{messages.length} messages to the queue", messages: messages)
-        messages.each { |message| @queue.push(message) }
-        log("Pushed #{messages.length} messages to the queue", messages: messages)
+        log("Pushing #{messages.length} messages to the queue",
+          logger: logger, threads: threads, queue: queue, messages: messages)
 
-        if @queue.length >= @queue_max
-          log("Sleeping for #{@poll} seconds because queue length >= queue max", messages: messages)
-          sleep(@poll)
-          log("slept for #{@poll} seconds", messages: messages)
+        messages.each { |message| queue.push(message) }
+
+        log("Pushed #{messages.length} messages to the queue",
+          logger: logger, threads: threads, queue: queue, messages: messages)
+
+        if queue.length >= queue_max
+          log("Sleeping for #{poll} seconds because queue length >= queue max",
+            logger: logger, threads: threads, queue: queue, messages: messages)
+
+          sleep(poll)
+
+          log("Slept for #{poll} seconds",
+            logger: logger, threads: threads, queue: queue, messages: messages)
         end
       end
-
-      @threads.length.times { @queue.push(nil) }
-
-      @threads.each(&:join)
     end
 
     def publish!(threads_max:, queue_max:, poll:, logger: nil, &block)
-      @threads_max = threads_max
-      @queue_max = queue_max
-      @poll = poll
-      @logger = logger
+      queue = Queue.new
 
-      @queue = Queue.new
-      @threads = []
+      threads = threads_max.times.map do
+        Thread.new do
+          loop do
+            pop_message!(queue: queue, logger: logger, &block)
+          rescue ThreadShutdown
+            break
+          rescue Exception => exception
+            logger.fatal "Critical Error: #{exception.class}: #{exception.message}"
 
-      @running = false
+            stop!
 
-      pop_messages_async!(&block)
+            break
+          end
+        end
+      end
 
-      push_messages!
+      push_messages!(threads: threads, queue: queue, queue_max: queue_max, poll: poll, logger: logger)
+
+      threads.length.times { queue.push(nil) }
+      threads.each(&:join)
     end
 
-    def log(message, messages:)
+    def log(message, logger:, threads:, queue:, messages:)
       summary = {
-        threads_max: @threads_max,
-        messages_length: messages.length,
-        queue_length: @queue.length
+        threads: threads.length,
+        messages: messages.length,
+        queue: queue.length
       }
 
-      @logger.debug "#{message} #{summary.to_json}"
+      logger.debug "#{message} #{summary.to_json}"
     end
   end
 end
