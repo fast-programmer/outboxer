ef34fb70faf36b9d09db6ecd33cacf4319cd43b1
bedrock-adam <adammikulas@gmail.com>
2024-01-26 19:40:58 +1100

    monitor whilst continuously adding jobs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
 require_relative '../app/models/invoice'
 require_relative '../app/models/event'
 
-500.times do |_i|
-  invoice = Invoice.create!
-  event = invoice.events.create!(type: 'Invoice.created')
+loop do
+  10.times do |_i|
+    invoice = Invoice.create!
+    event = invoice.events.create!(type: 'Invoice.created')
 
-  puts "Invoice##{invoice.id} - #{event.type} event #{event.id}"
+    puts "Invoice##{invoice.id} - #{event.type} event #{event.id}"
+  end
+
+  sleep 1
 end
