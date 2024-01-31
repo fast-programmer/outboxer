7c688b440be82d49125b1aa2044a4545e4ea521a
bedrock-adam <adammikulas@gmail.com>
2024-02-01 00:15:11 +1100

    rescue socket connection errors too

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,7 +24,7 @@ RSpec.configure do |config|
   config.after(:each) do
     begin
       DatabaseCleaner.clean
-    rescue ActiveRecord::DatabaseConnectionError
+    rescue ActiveRecord::DatabaseConnectionError, ActiveRecord::ConnectionNotEstablished
       # ignore
     end
   end
