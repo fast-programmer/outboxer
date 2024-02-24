5edc08993209c690ed00b92830021be4425e4b8e
bedrock-adam <adammikulas@gmail.com>
2024-02-25 02:30:04 +1100

    fix show bugs

--- web/app.rb
+++ web/app.rb
@@ -17,64 +17,6 @@ module Outboxer
   class App < Sinatra::Base
     set :method_override, true
 
-    post '/messages/search' do
-      text = params['text']
-
-      binding.pry
-
-      redirect to('/messages')
-    end
-
-    post '/messages/per_page' do
-      page_number = params[:page_number] || 1
-      per_page = [100, 200, 500, 1000].include?(params[:per_page].to_i) ? params[:per_page].to_i : 100
-      order = %w[id status messageable created_at updated_at]
-        .include?(params[:order]) ? params[:order].to_sym : :created_at
-      sort = %w[asc desc].include?(params[:sort]) ? params[:sort].to_sym : :asc
-
-      redirect "/messages?" \
-        "#{URI.encode_www_form_component('order')}=#{URI.encode_www_form_component(order)}&" \
-        "#{URI.encode_www_form_component('sort')}=#{URI.encode_www_form_component(sort)}&" \
-        "#{URI.encode_www_form_component('page_number')}=#{URI.encode_www_form_component(page_number)}&" \
-        "#{URI.encode_www_form_component('per_page')}=#{URI.encode_www_form_component(per_page)}"
-    end
-
-    get '/messages' do
-      messages = Models::Message
-
-      page_number = params[:page_number] || 1
-      messages = messages.page(page_number)
-
-      per_page = [100, 200, 500, 1000].include?(params[:per_page].to_i) ? params[:per_page].to_i : 100
-      messages = messages.per(per_page)
-
-      order = %w[asc desc].include?(params[:order]) ? params[:order].to_sym : :asc
-
-      sort = %w[id status messageable created_at updated_at]
-        .include?(params[:sort]) ? params[:sort].to_sym : :created_at
-
-      if sort == :messageable
-        messages.order(messageable_type: order, messageable_id: order).to_sql
-      else
-        messages = messages.order(sort => order)
-      end
-
-      messages_count = Models::Message.count
-
-      status_counts = { 'unpublished' => 0, 'publishing' => 0, 'failed' => 0 }.merge(
-        Models::Message.group(:status).count)
-
-      erb :messages, layout: nil, locals: {
-        status_counts: status_counts,
-        messages_count: messages_count,
-        messages: messages,
-        order: order,
-        sort: sort,
-        page_number: page_number,
-        per_page: per_page
-      }
-    end
-
     get '/messages/unpublished' do
       page_number = params[:page_number] || 1
       per_page = [100, 200, 500, 1000].include?(params[:per_page].to_i) ? params[:per_page].to_i : 100
@@ -151,6 +93,80 @@ module Outboxer
     end
 
 
+    get '/messages/:id' do |id|
+      messages_count = Models::Message.count
+
+      message = Models::Message.includes(:exceptions).find(id)
+      status_counts = { 'unpublished' => 0, 'publishing' => 0, 'failed' => 0 }.merge(
+        Models::Message.group(:status).count)
+
+      erb :'messages/show', locals: {
+        status_counts: status_counts,
+        messages_count: messages_count,
+        message: message
+      }
+    end
+
+
+    post '/messages/search' do
+      text = params['text']
+
+      binding.pry
+
+      redirect to('/messages')
+    end
+
+    post '/messages/per_page' do
+      page_number = params[:page_number] || 1
+      per_page = [100, 200, 500, 1000].include?(params[:per_page].to_i) ? params[:per_page].to_i : 100
+      order = %w[id status messageable created_at updated_at]
+        .include?(params[:order]) ? params[:order].to_sym : :created_at
+      sort = %w[asc desc].include?(params[:sort]) ? params[:sort].to_sym : :asc
+
+      redirect "/messages?" \
+        "#{URI.encode_www_form_component('order')}=#{URI.encode_www_form_component(order)}&" \
+        "#{URI.encode_www_form_component('sort')}=#{URI.encode_www_form_component(sort)}&" \
+        "#{URI.encode_www_form_component('page_number')}=#{URI.encode_www_form_component(page_number)}&" \
+        "#{URI.encode_www_form_component('per_page')}=#{URI.encode_www_form_component(per_page)}"
+    end
+
+    get '/messages' do
+      messages = Models::Message
+
+      page_number = params[:page_number] || 1
+      messages = messages.page(page_number)
+
+      per_page = [100, 200, 500, 1000].include?(params[:per_page].to_i) ? params[:per_page].to_i : 100
+      messages = messages.per(per_page)
+
+      order = %w[asc desc].include?(params[:order]) ? params[:order].to_sym : :asc
+
+      sort = %w[id status messageable created_at updated_at]
+        .include?(params[:sort]) ? params[:sort].to_sym : :created_at
+
+      if sort == :messageable
+        messages.order(messageable_type: order, messageable_id: order).to_sql
+      else
+        messages = messages.order(sort => order)
+      end
+
+      messages_count = Models::Message.count
+
+      status_counts = { 'unpublished' => 0, 'publishing' => 0, 'failed' => 0 }.merge(
+        Models::Message.group(:status).count)
+
+      erb :messages, layout: nil, locals: {
+        status_counts: status_counts,
+        messages_count: messages_count,
+        messages: messages,
+        order: order,
+        sort: sort,
+        page_number: page_number,
+        per_page: per_page
+      }
+    end
+
+
     post '/messages/bulk_action' do
       bulk_action = params['bulk_action']
       message_ids = params['message_ids']
@@ -202,20 +218,6 @@ module Outboxer
     get '/' do
       redirect to('/messages/all')
     end
-
-    # get '/messages/:id' do |id|
-    #   messages_count = Models::Message.count
-
-    #   message = Models::Message.includes(:outboxer_exceptions).find(id)
-    #   status_counts = { 'unpublished' => 0, 'publishing' => 0, 'failed' => 0 }.merge(
-    #     Models::Message.group(:status).count)
-
-    #   erb :'messages/show', locals: {
-    #     status_counts: status_counts,
-    #     messages_count: messages_count,
-    #     message: message
-    #   }
-    # end
 
     # get '/:status' do |status|
     #   page = params[:page] || 1
