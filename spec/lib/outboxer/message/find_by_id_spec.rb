79aee414ae2ef9fdf304a002b762f854480c3e57
bedrock-adam <adammikulas@gmail.com>
2024-03-16 16:29:29 +1100

    pass all specs

--- spec/lib/outboxer/message/find_by_id_spec.rb
+++ spec/lib/outboxer/message/find_by_id_spec.rb
@@ -3,16 +3,16 @@ require 'spec_helper'
 module Outboxer
   RSpec.describe Message do
     describe '.find_by_id!' do
-      let!(:message) { create(:message, id: 1, status: 'sent', created_at: 1.day.ago, updated_at: 1.hour.ago) }
-      let!(:exception) { create(:exception, message: message) }
-      let!(:frame) { create(:frame, exception: exception) }
+      context 'when a failed message exists' do
+        let!(:message) { create(:outboxer_message, :failed) }
+        let!(:exception) { create(:outboxer_exception, message: message) }
+        let!(:frame) { create(:outboxer_frame, exception: exception) }
 
-      context 'when the message exists' do
-        it 'returns the message details with associated exceptions and frames' do
+        it 'returns the message, exceptions and frames' do
           result = Message.find_by_id!(id: message.id)
 
           expect(result['id']).to eq(message.id)
-          expect(result['status']).to eq('sent')
+          expect(result['status']).to eq('failed')
           expect(result['created_at']).to eq(message.created_at.utc.to_s)
           expect(result['updated_at']).to eq(message.updated_at.utc.to_s)
           expect(result['exceptions'].first['id']).to eq(exception.id)
