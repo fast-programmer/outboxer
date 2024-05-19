d0a47d571bbb836a1dcbd23d33c99dbe86e23fc2
bedrock-adam <adammikulas@gmail.com>
2024-05-19 21:18:35 +1000

    when no arguments specified

--- spec/lib/outboxer/messages/list/filter_spec.rb
+++ spec/lib/outboxer/messages/list/filter_spec.rb
@@ -32,56 +32,6 @@ module Outboxer
         end
       end
 
-      context 'with no status specified' do
-        it 'returns all messages' do
-          expect(Messages.list).to eq({
-            current_page: 1, limit_value: 100, total_count: 5, total_pages: 1,
-            messages: [
-              {
-                id: message_1.id,
-                status: message_1.status.to_sym,
-                messageable_type: message_1.messageable_type,
-                messageable_id: message_1.messageable_id,
-                created_at: message_1.created_at,
-                updated_at: message_1.updated_at
-              },
-              {
-                id: message_2.id,
-                status: message_2.status.to_sym,
-                messageable_type: message_2.messageable_type,
-                messageable_id: message_2.messageable_id,
-                created_at: message_2.created_at,
-                updated_at: message_2.updated_at
-              },
-              {
-                id: message_3.id,
-                status: message_3.status.to_sym,
-                messageable_type: message_3.messageable_type,
-                messageable_id: message_3.messageable_id,
-                created_at: message_3.created_at,
-                updated_at: message_3.updated_at
-              },
-              {
-                id: message_4.id,
-                status: message_4.status.to_sym,
-                messageable_type: message_4.messageable_type,
-                messageable_id: message_4.messageable_id,
-                created_at: message_4.created_at,
-                updated_at: message_4.updated_at
-              },
-              {
-                id: message_5.id,
-                status: message_5.status.to_sym,
-                messageable_type: message_5.messageable_type,
-                messageable_id: message_5.messageable_id,
-                created_at: message_5.created_at,
-                updated_at: message_5.updated_at
-              }
-            ]
-          })
-        end
-      end
-
       context 'with backlogged status' do
         it 'returns backlogged messages' do
           expect(Messages.list(status: :backlogged)).to eq({
