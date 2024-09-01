77799f3728166b9413634630f0be9745dd310084
bedrock-adam <adammikulas@gmail.com>
2024-09-01 16:30:06 +1000

    add specs

--- lib/outboxer/single_threaded_publisher.rb
+++ lib/outboxer/single_threaded_publisher.rb
@@ -4,34 +4,60 @@ module Outboxer
 
     @publishing = false
 
-    def publish(
-      batch_size: 200, poll_interval: 0.1,
-      logger: Logger.new($stdout, level: Logger::INFO),
-      kernel: Kernel,
-      time: Time,
-      &block
-    )
-      @publishing = true
-
-      dequeue_messages(
-        batch_size: batch_size,
-        poll_interval: poll_interval,
-        logger: logger,
-        time: time,
-        kernel: kernel,
-        &block)
-    end
-
     def stop
       @publishing = false
     end
 
-    private
+    def sleep(duration, start_time:, tick_interval:, time:, kernel:)
+      while @publishing && (time.now - start_time) < duration
+        kernel.sleep(tick_interval)
+      end
+    end
 
-    def process_message(queued_message:, logger:, time:, kernel:, &block)
-      dequeued_at = queued_message[:dequeued_at]
+    def publish(
+      batch_size: 200, poll_interval: 5, tick_interval: 1,
+      logger: Logger.new($stdout, level: Logger::INFO),
+      time: Time, kernel: Kernel,
+      &block
+    )
+      logger.info "Dequeueing up to #{batch_size} messages every #{poll_interval} second(s)"
 
-      message = Message.publishing(id: queued_message[:id])
+      @publishing = true
+
+      while @publishing
+        begin
+          dequeued_messages = Messages.dequeue(limit: batch_size)
+
+          dequeued_messages.each do |dequeued_message|
+            publish_message(
+              dequeued_message: { id: dequeued_message[:id], dequeued_at: time.now.utc },
+              logger: logger, time: time, kernel: kernel,
+              &block)
+          end
+
+          if dequeued_messages.count < batch_size
+            Publisher.sleep(
+              poll_interval, start_time: time.now, tick_interval: tick_interval,
+              time: time, kernel: kernel)
+          end
+        rescue StandardError => exception
+          logger.error "#{exception.class}: #{exception.message}"
+          exception.backtrace.each { |frame| logger.error frame }
+        rescue Exception => exception
+          logger.fatal "#{exception.class}: #{exception.message}"
+          exception.backtrace.each { |frame| logger.fatal frame }
+
+          stop
+        end
+      end
+
+      logger.info "Stopped dequeueing messages"
+    end
+
+    def publish_message(dequeued_message:, logger:, time:, kernel:, &block)
+      dequeued_at = dequeued_message[:dequeued_at]
+
+      message = Message.publishing(id: dequeued_message[:id])
       logger.debug "Publishing message #{message[:id]} for " \
         "#{message[:messageable_type]}::#{message[:messageable_id]} " \
         "in #{(time.now.utc - dequeued_at).round(3)}s"
@@ -62,45 +88,6 @@ module Outboxer
       exception.backtrace.each { |frame| logger.error frame }
 
       stop
-    end
-
-    def sleep(duration, poll_interval: 0.1, start_time: Time.now, time: Time, kernel: Kernel)
-      while @publishing && (time.now - start_time) < duration
-        kernel.sleep(poll_interval)
-      end
-    end
-
-    def dequeue_messages(batch_size:, poll_interval:,
-                         logger:, time:, kernel:, &block)
-      logger.info "Dequeueing up to #{batch_size} messages every #{poll_interval} second(s)"
-
-      while @publishing
-        begin
-          dequeued_messages = Messages.dequeue(limit: batch_size)
-
-          dequeued_messages.each do |message|
-            process_message(
-              queued_message: { id: message[:id], dequeued_at: time.now.utc },
-              logger: logger, time: time, kernel: kernel,
-              &block)
-          end
-
-          if dequeued_messages.size < batch_size
-            Publisher.sleep(poll_interval, time: time, kernel: kernel)
-          end
-        rescue StandardError => exception
-          logger.error "#{exception.class}: #{exception.message}"
-          exception.backtrace.each { |frame| logger.error frame }
-
-        rescue Exception => exception
-          logger.fatal "#{exception.class}: #{exception.message}"
-          exception.backtrace.each { |frame| logger.fatal frame }
-
-          stop
-        end
-      end
-
-      logger.info "Stopped polling database"
     end
   end
 end
