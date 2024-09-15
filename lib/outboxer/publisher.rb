b4cf83397b96522804cd7dc66a5a053b779c2283
bedrock-adam <adammikulas@gmail.com>
2024-09-15 20:13:05 +1000

    revert to single threaded implementation

--- lib/outboxer/publisher.rb
+++ lib/outboxer/publisher.rb
@@ -36,38 +36,25 @@ module Outboxer
     end
 
     def publish(
-      batch_size: 200, poll_interval: 5, tick_interval: 0.1, num_worker_threads: 1,
+      batch_size: 200, poll_interval: 5, tick_interval: 0.1,
       logger: Logger.new($stdout, level: Logger::INFO),
       time: Time, kernel: Kernel, &block
     )
       logger.info "Outboxer v#{Outboxer::VERSION} publishing in ruby #{RUBY_VERSION} " \
         "(#{RUBY_RELEASE_DATE} revision #{RUBY_REVISION[0, 10]}) [#{RUBY_PLATFORM}]"
 
-      logger.info "Outboxer dequeueing { batch_size: #{batch_size}, poll_interval: #{poll_interval}, " \
-        "workers: #{num_worker_threads} }"
+      logger.info "Outboxer dequeueing { batch_size: #{batch_size}, poll_interval: #{poll_interval} } "
       @status = Status::PUBLISHING
-
-      worker_threads = num_worker_threads.times.map do
-        Thread.new do
-          while (message = @queue.pop)
-            break if message.nil?
-            publish_message(dequeued_message: message, logger: logger, time: time, kernel: kernel, &block)
-          end
-        end
-      end
 
       while @status != Status::TERMINATING
         case @status
         when Status::PUBLISHING
           begin
             dequeued_messages = Messages.dequeue(limit: batch_size, logger: logger)
-            dequeued_messages.each do |msg|
-              while @queue.size >= batch_size
-                Publisher.sleep(tick_interval, start_time: time.now, tick_interval: tick_interval,
-                                time: time, kernel: kernel)
-              end
-              @queue.push(msg)
+            dequeued_messages.each do |message|
+              publish_message(dequeued_message: message, logger: logger, time: time, kernel: kernel, &block)
             end
+
             if dequeued_messages.count < batch_size
               Publisher.sleep(poll_interval, start_time: time.now, tick_interval: tick_interval,
                               time: time, kernel: kernel)
@@ -81,13 +68,11 @@ module Outboxer
             terminate
           end
         when Status::PAUSED
-          Publisher.sleep(poll_interval, start_time: time.now, tick_interval: tick_interval,
+          Publisher.sleep(tick_interval, start_time: time.now, tick_interval: tick_interval,
                           time: time, kernel: kernel)
         end
       end
 
-      num_worker_threads.times { @queue.push(nil) }
-      worker_threads.each(&:join)
       logger.info "Outboxer stopped dequeueing messages"
     end
 
