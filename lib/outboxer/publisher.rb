9a633b22c1330871b3f2b990cec8043d4300275b
bedrock-adam <adammikulas@gmail.com>
2024-09-23 20:01:27 +1000

    fix multipe line string

--- lib/outboxer/publisher.rb
+++ lib/outboxer/publisher.rb
@@ -42,7 +42,7 @@ module Outboxer
       logger: Logger.new($stdout, level: Logger::INFO),
       time: Time, process: ::Process, kernel: Kernel, &block
     )
-      logger.info "Outboxer v#{Outboxer::VERSION} publishing in ruby #{RUBY_VERSION} " \
+      logger.info "Outboxer v#{Outboxer::VERSION} publishing in ruby #{RUBY_VERSION} "\
         "(#{RUBY_RELEASE_DATE} revision #{RUBY_REVISION[0, 10]}) [#{RUBY_PLATFORM}]"
 
       logger.info "Outboxer dequeueing "\
@@ -68,7 +68,7 @@ module Outboxer
               publishing_end_time = process.clock_gettime(process::CLOCK_MONOTONIC)
               publishing_time = ((publishing_end_time - publishing_start_time))
 
-              logger.debug "Outboxer published #{dequeued_messages.count} messages in " \
+              logger.debug "Outboxer published #{dequeued_messages.count} messages in "\
                 "#{(publishing_time * 1000).round(2)}ms " \
                 "(#{((publishing_time / dequeued_messages.count) * 1000).round(2)}ms per message)"
             end
@@ -105,24 +105,24 @@ module Outboxer
       dequeued_at = dequeued_message[:updated_at]
 
       message = Message.publishing(id: dequeued_message[:id])
-      logger.debug "Outboxer publishing message #{message[:id]} for " \
-        "#{message[:messageable_type]}::#{message[:messageable_id]} " \
+      logger.debug "Outboxer publishing message #{message[:id]} for "\
+        "#{message[:messageable_type]}::#{message[:messageable_id]} "\
         "in #{(time.now.utc - dequeued_at).round(3)}s"
 
       begin
         block.call(message)
       rescue Exception => exception
         Message.failed(id: message[:id], exception: exception)
-        logger.error "Outboxer failed to publish message #{message[:id]} for " \
-          "#{message[:messageable_type]}::#{message[:messageable_id]} " \
+        logger.error "Outboxer failed to publish message #{message[:id]} for "\
+          "#{message[:messageable_type]}::#{message[:messageable_id]} "\
           "in #{(time.now.utc - dequeued_at).round(3)}s"
 
         raise
       end
 
       Message.published(id: message[:id])
-      logger.debug "Outboxer published message #{message[:id]} for " \
-        "#{message[:messageable_type]}::#{message[:messageable_id]} " \
+      logger.debug "Outboxer published message #{message[:id]} for "\
+        "#{message[:messageable_type]}::#{message[:messageable_id]} "\
         "in #{(time.now.utc - dequeued_at).round(3)}s"
     rescue StandardError => exception
       logger.error "#{exception.class}: #{exception.message} "\
