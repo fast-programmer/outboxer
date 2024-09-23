673532aaa9ae3c7909324ba000e1c9cd4a88e6bc
Adam Mikulasev <adammikulas@gmail.com>
2024-09-23 20:08:36 +1000

    add resumable single threaded publisher (#129)

--- Procfile
+++ Procfile
@@ -1,5 +1,6 @@
-# outboxer_message_queuer: bundle exec ruby script/message/queuer.rb
-sidekiq: bundle exec sidekiq -C config/sidekiq.yml -r ./config/sidekiq.rb
-outboxer_published_messages_deleter: bin/outboxer_published_messages_deleter
-outboxer_publisher: bin/outboxer_publisher
-outboxer_app: bundle exec rerun "rackup -p 4567"
+# outboxer_message_queuer: bundle exec ruby script/message/queuer.rb 100000
+# sidekiq: bundle exec sidekiq -C config/sidekiq.yml -r ./config/sidekiq.rb
+# outboxer_published_messages_deleter: bin/outboxer_published_messages_deleter
+# outboxer_publisher: bin/outboxer_publisher
+# outboxer_app: bundle exec rerun "rackup -p 4567"
+outboxer_publisher_1: bin/outboxer_publisher
--- lib/outboxer/publisher.rb
+++ lib/outboxer/publisher.rb
@@ -2,54 +2,100 @@ module Outboxer
   module Publisher
     extend self
 
-    @publishing = false
+    module Status
+      PAUSED = :paused
+      PUBLISHING = :publishing
+      TERMINATING = :terminating
+    end
 
-    def stop
-      @publishing = false
+    STATUSES = [Status::PAUSED, Status::PUBLISHING, Status::TERMINATING]
+    @status = Status::PAUSED
+
+    Signal.trap("TERM") { terminate }
+    Signal.trap("INT")  { terminate }
+    Signal.trap("TSTP") { pause }
+    Signal.trap("CONT") { resume }
+
+    def terminate
+      @status = Status::TERMINATING
+    end
+
+    def pause
+      @status = Status::PAUSED
+    end
+
+    def resume
+      @status = Status::PUBLISHING
     end
 
     # :nocov:
-    def sleep(duration, start_time:, tick_interval:, time:, kernel:)
-      while @publishing && (time.now - start_time) < duration
+    def sleep(duration, start_time:, tick_interval:, process:, kernel:)
+      while (@status != Status::TERMINATING) &&
+          (process.clock_gettime(process::CLOCK_MONOTONIC) - start_time) < duration
         kernel.sleep(tick_interval)
       end
     end
     # :nocov:
 
     def publish(
-      batch_size: 200, poll_interval: 5, tick_interval: 1,
+      batch_size: 200, poll_interval: 5, tick_interval: 0.1,
       logger: Logger.new($stdout, level: Logger::INFO),
-      time: Time, kernel: Kernel,
+      time: Time, process: ::Process, kernel: Kernel,
       &block
     )
-      logger.info "Outboxer dequeueing messages " \
-        "{ batch_size: #{batch_size}, poll_interval: #{poll_interval}, " \
-        "tick_interval: #{tick_interval} }"
+      logger.info "Outboxer v#{Outboxer::VERSION} publishing in ruby #{RUBY_VERSION} "\
+        "(#{RUBY_RELEASE_DATE} revision #{RUBY_REVISION[0, 10]}) [#{RUBY_PLATFORM}]"
 
-      @publishing = true
+      logger.info "Outboxer dequeueing "\
+                  "{ batch_size: #{batch_size}, poll_interval: #{poll_interval} } "
+      @status = Status::PUBLISHING
 
-      while @publishing
-        begin
-          dequeued_messages = Messages.dequeue(limit: batch_size)
+      while @status != Status::TERMINATING
+        case @status
+        when Status::PUBLISHING
+          begin
+            publishing_start_time = process.clock_gettime(process::CLOCK_MONOTONIC)
 
-          dequeued_messages.each do |dequeued_message|
-            publish_message(
-              dequeued_message: dequeued_message, logger: logger, time: time, kernel: kernel, &block)
+            dequeued_messages = Messages.dequeue(limit: batch_size)
+
+            if dequeued_messages.count > 0
+              dequeued_messages.each do |message|
+                publish_message(
+                  dequeued_message: message,
+                  logger: logger, time: time, kernel: kernel,
+                  &block)
+              end
+
+              publishing_end_time = process.clock_gettime(process::CLOCK_MONOTONIC)
+              publishing_time = ((publishing_end_time - publishing_start_time))
+
+              logger.debug "Outboxer published #{dequeued_messages.count} messages in "\
+                "#{(publishing_time * 1000).round(2)}ms " \
+                "(#{((publishing_time / dequeued_messages.count) * 1000).round(2)}ms per message)"
+            end
+
+            if dequeued_messages.count < batch_size
+              Publisher.sleep(
+                poll_interval,
+                start_time: process.clock_gettime(process::CLOCK_MONOTONIC),
+                tick_interval: tick_interval,
+                process: process, kernel: kernel)
+            end
+          rescue StandardError => exception
+            logger.error "#{exception.class}: #{exception.message}"
+            exception.backtrace.each { |frame| logger.error frame }
+          rescue Exception => exception
+            logger.fatal "#{exception.class}: #{exception.message}"
+            exception.backtrace.each { |frame| logger.fatal frame }
+
+            terminate
           end
-
-          if dequeued_messages.count < batch_size
-            Publisher.sleep(
-              poll_interval, start_time: time.now, tick_interval: tick_interval,
-              time: time, kernel: kernel)
-          end
-        rescue StandardError => exception
-          logger.error "#{exception.class}: #{exception.message}"
-          exception.backtrace.each { |frame| logger.error frame }
-        rescue Exception => exception
-          logger.fatal "#{exception.class}: #{exception.message}"
-          exception.backtrace.each { |frame| logger.fatal frame }
-
-          stop
+        when Status::PAUSED
+          Publisher.sleep(
+            tick_interval,
+            start_time: process.clock_gettime(process::CLOCK_MONOTONIC),
+            tick_interval: tick_interval,
+            process: process, kernel: kernel)
         end
       end
 
@@ -60,36 +106,35 @@ module Outboxer
       dequeued_at = dequeued_message[:updated_at]
 
       message = Message.publishing(id: dequeued_message[:id])
-      logger.debug "Outboxer publishing message #{message[:id]} for " \
-        "#{message[:messageable_type]}::#{message[:messageable_id]} " \
+      logger.debug "Outboxer publishing message #{message[:id]} for "\
+        "#{message[:messageable_type]}::#{message[:messageable_id]} "\
         "in #{(time.now.utc - dequeued_at).round(3)}s"
 
       begin
         block.call(message)
       rescue Exception => exception
         Message.failed(id: message[:id], exception: exception)
-        logger.error "Outboxer failed to publish message #{message[:id]} for " \
-          "#{message[:messageable_type]}::#{message[:messageable_id]} " \
+        logger.error "Outboxer failed to publish message #{message[:id]} for "\
+          "#{message[:messageable_type]}::#{message[:messageable_id]} "\
           "in #{(time.now.utc - dequeued_at).round(3)}s"
 
         raise
       end
 
       Message.published(id: message[:id])
-      logger.debug "Outboxer published message #{message[:id]} for " \
-        "#{message[:messageable_type]}::#{message[:messageable_id]} " \
+      logger.debug "Outboxer published message #{message[:id]} for "\
+        "#{message[:messageable_type]}::#{message[:messageable_id]} "\
         "in #{(time.now.utc - dequeued_at).round(3)}s"
     rescue StandardError => exception
-      logger.error "#{exception.class}: #{exception.message} " \
+      logger.error "#{exception.class}: #{exception.message} "\
         "in #{(time.now.utc - dequeued_at).round(3)}s"
-
       exception.backtrace.each { |frame| logger.error frame }
     rescue Exception => exception
-      logger.fatal "#{exception.class}: #{exception.message} " \
+      logger.fatal "#{exception.class}: #{exception.message} "\
         "in #{(time.now.utc - dequeued_at).round(3)}s"
-      exception.backtrace.each { |frame| logger.error frame }
+      exception.backtrace.each { |frame| logger.fatal frame }
 
-      stop
+      terminate
     end
   end
 end
