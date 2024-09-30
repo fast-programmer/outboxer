6739956e7f151918b1347d07a241564e1a8b2f9f
bedrock-adam <adammikulas@gmail.com>
2024-09-30 22:54:34 +1000

    fix issues

--- lib/outboxer/web.rb
+++ lib/outboxer/web.rb
@@ -439,6 +439,14 @@ module Outboxer
         per_page: denormalised_query_params[:per_page],
         time_zone: denormalised_query_params[:time_zone])
 
+      normalised_query_string = normalise_query_string(
+        status: denormalised_query_params[:status],
+        sort: denormalised_query_params[:sort],
+        order: denormalised_query_params[:order],
+        page: denormalised_query_params[:page],
+        per_page: denormalised_query_params[:per_page],
+        time_zone: denormalised_query_params[:time_zone])
+
       messages_metrics = Messages.metrics
 
       message = Message.find_by_id(id: params[:id])
@@ -449,6 +457,7 @@ module Outboxer
       erb :message, locals: {
         denormalised_query_params: denormalised_query_params,
         normalised_query_params: normalised_query_params,
+        normalised_query_string: normalised_query_string,
         messages_metrics: messages_metrics,
         message: message,
         messageable: messageable
