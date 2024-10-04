4e2c1a498b7eae74f8300a68f121480881515e55
Adam Mikulasev <adammikulas@gmail.com>
2024-10-04 22:21:37 +1000

    add invalid time_zone test coverage to Messages.list (#142)

---
+++ spec/lib/outboxer/messages/list/time_zone_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+module Outboxer
+  RSpec.describe Messages do
+    describe '.list' do
+      context 'when time_zone invalid' do
+        it 'raises an ArgumentError' do
+          expect do
+            Messages.list(time_zone: 'invalid')
+          end.to raise_error(ArgumentError, "time_zone must be valid")
+        end
+      end
+    end
+  end
+end
