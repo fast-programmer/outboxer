9b3d3c348dce2f9140b13395aa1c43f4605b8c70
bedrock-adam <adammikulas@gmail.com>
2024-11-03 20:16:13 +1100

    cherry-pick from previous work

--- lib/outboxer.rb
+++ lib/outboxer.rb
@@ -11,6 +11,7 @@ require_relative "outboxer/models/exception"
 require_relative "outboxer/models/message"
 
 require_relative "outboxer/models/publisher"
+require_relative "outboxer/models/signal"
 
 require_relative "outboxer/socket"
 require_relative "outboxer/process"
