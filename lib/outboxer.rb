ab301785b6a8d4ebb463763585efa40e9a0fc3c1
bedrock-adam <adammikulas@gmail.com>
2024-11-09 17:50:57 +1100

    remove unused module

--- lib/outboxer.rb
+++ lib/outboxer.rb
@@ -13,7 +13,6 @@ require_relative "outboxer/models/message"
 require_relative "outboxer/models/publisher"
 require_relative "outboxer/models/signal"
 
-require_relative "outboxer/socket"
 require_relative "outboxer/process"
 
 require_relative "outboxer/logger"
