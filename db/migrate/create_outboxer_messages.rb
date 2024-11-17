18a4b2a8d7e855278aa62bda4f858a30596e9dd9
bedrock-adam <adammikulas@gmail.com>
2024-11-17 19:00:19 +1100

    replace t.timestamps with explicit columns

--- db/migrate/create_outboxer_messages.rb
+++ db/migrate/create_outboxer_messages.rb
@@ -6,7 +6,8 @@ class CreateOutboxerMessages < ActiveRecord::Migration[6.1]
       t.string :messageable_id, limit: 255, null: false
       t.string :messageable_type, limit: 255, null: false
 
-      t.timestamps
+      t.datetime :created_at, precision: 6, null: false
+      t.datetime :updated_at, precision: 6, null: false
 
       t.string :updated_by_publisher_name, limit: 263 # 255 (hostname) + 1 (colon) + 7 (pid)
       t.bigint :updated_by_publisher_id
