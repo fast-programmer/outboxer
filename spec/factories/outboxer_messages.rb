d8c665ea45ee642bf3b498e250f98e60d2f3cdcd
bedrock-adam <adammikulas@gmail.com>
2024-11-22 22:07:45 +1100

    fix build

--- spec/factories/outboxer_messages.rb
+++ spec/factories/outboxer_messages.rb
@@ -2,52 +2,57 @@ FactoryBot.define do
   factory :outboxer_message, class: 'Outboxer::Models::Message' do
     messageable_type { 'Event' }
     messageable_id { 1 }
-    status { Outboxer::Models::Message::Status::QUEUED }
-    queued_at { 1.day.ago }
-    updated_at { 1.day.ago }
+
+    status { Outboxer::Models::Message::Status::PUBLISHED }
+    queued_at { 10.seconds.ago }
+    buffered_at { 9.seconds.ago }
+    publishing_at { 8.seconds.ago }
+    updated_at { 7.seconds.ago }
+
     updated_by_publisher_id { 5 }
     updated_by_publisher_name { 'server-01:47000' }
 
     trait :queued do
       status { Outboxer::Models::Message::Status::QUEUED }
 
-      queued_at { 1.day.ago }
+      queued_at { 10.seconds.ago }
+      updated_at { 10.seconds.ago }
     end
 
     trait :buffered do
       status { Outboxer::Models::Message::Status::BUFFERED }
 
-      queued_at { 24.hours.ago }
-      buffered_at { 22.hours.ago }
+      queued_at { 10.seconds.ago }
+      buffered_at { 9.second.ago }
       publishing_at { nil }
-      updated_at { 22.hours.ago }
+      updated_at { 9.second.ago }
     end
 
     trait :publishing do
       status { Outboxer::Models::Message::Status::PUBLISHING }
 
-      queued_at { 24.hours.ago }
-      buffered_at { 22.hours.ago }
-      publishing_at { 23.hours.ago }
-      updated_at { 22.hours.ago }
+      queued_at { 10.seconds.ago }
+      buffered_at { 9.seconds.ago }
+      publishing_at { 8.seconds.ago }
+      updated_at { 8.seconds.ago }
     end
 
     trait :published do
       status { Outboxer::Models::Message::Status::PUBLISHED }
 
-      queued_at { 24.hours.ago }
-      buffered_at { 22.hours.ago }
-      publishing_at { 23.hours.ago }
-      updated_at { 24.hours.ago }
+      queued_at { 10.seconds.ago }
+      buffered_at { 9.seconds.ago }
+      publishing_at { 8.seconds.ago }
+      updated_at { 7.seconds.ago }
     end
 
     trait :failed do
       status { Outboxer::Models::Message::Status::FAILED }
 
-      queued_at { 24.hours.ago }
-      buffered_at { 22.hours.ago }
-      publishing_at { 23.hours.ago }
-      updated_at { 24.hours.ago }
+      queued_at { 10.seconds.ago }
+      buffered_at { 9.seconds.ago }
+      publishing_at { 8.seconds.ago }
+      updated_at { 7.seconds.ago }
     end
   end
 end
