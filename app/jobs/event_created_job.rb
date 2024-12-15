032251b0757572979dd90f4130641861308fdc1b
bedrock-adam <adammikulas@gmail.com>
2024-12-15 15:01:08 +1100

    update sidekiq publisher generator

--- app/jobs/event_created_job.rb
+++
@@ -1,7 +0,0 @@
-class EventCreatedJob
-  include Sidekiq::Job
-
-  def perform(args)
-    Sidekiq.logger.info "EventCreatedJob#perform args: #{args.inspect}"
-  end
-end
---
+++ app/jobs/outboxer_integration/message/publish_job.rb
@@ -0,0 +1,11 @@
+module OutboxerIntegration
+  module Message
+    class PublishJob
+      include Sidekiq::Job
+
+      def perform(args)
+        Sidekiq.logger.info "OutboxerIntegration::Message::PublishJob#perform args: #{args.inspect}"
+      end
+    end
+  end
+end
