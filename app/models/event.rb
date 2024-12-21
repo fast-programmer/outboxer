4f67433c2989b980efd5273fe9cb20257e15abe8
bedrock-adam <adammikulas@gmail.com>
2024-12-21 17:55:05 +1100

    update based on readme

--- app/models/event.rb
+++ app/models/event.rb
@@ -16,9 +16,28 @@ class Event < ActiveRecord::Base
 
   # belongs_to :eventable, polymorphic: true
 
+end
+
+class Event < ActiveRecord::Base
+  self.table_name = 'events'
+
+  # associations
+
+  belongs_to :eventable, polymorphic: true
+
   # callbacks
 
   after_create do |event|
     Outboxer::Message.queue(messageable: event)
   end
+
+  # validations
+
+  validates :user_id, presence: true
+  validates :tenant_id, presence: true
+
+  validates :type, presence: true, length: { maximum: 255 }
+
+  validates :eventable_type, presence: true, length: { maximum: 255 }, if: -> { eventable_id.present? }
+  validates :eventable_id, presence: true, if: -> { eventable_type.present? }
 end
