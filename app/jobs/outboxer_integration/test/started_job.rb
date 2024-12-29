63601cd5bc2a91a10ad1b138abae1543e94e3333
Adam Mikulasev <adammikulas@gmail.com>
2024-12-29 21:37:57 +1100

    add application services on model (#217)

--- app/jobs/outboxer_integration/test/started_job.rb
+++ app/jobs/outboxer_integration/test/started_job.rb
@@ -4,20 +4,7 @@ module OutboxerIntegration
       include Sidekiq::Job
 
       def perform(args)
-        ActiveRecord::Base.transaction do
-          started_event = Test::StartedEvent.find(args['event_id'])
-
-          test = Test.find(started_event.eventable_id)
-          test.touch
-
-          Test::CompletedEvent.create!(
-            user_id: started_event.user_id,
-            tenant_id: started_event.tenant_id,
-            eventable: test,
-            body: {
-              'test' => {
-                'id' => started_event.body['test']['id'] } })
-        end
+        Test.complete(event_id: args['event_id'])
       end
     end
   end
--- app/models/outboxer_integration/test.rb
+++ app/models/outboxer_integration/test.rb
@@ -2,6 +2,40 @@ module OutboxerIntegration
   class Test < ActiveRecord::Base
     self.table_name = 'outboxer_integration_tests'
 
+    def self.start(user_id: nil, tenant_id: nil)
+      transaction do
+        test = create!(tenant_id: tenant_id)
+
+        event = StartedEvent.create!(
+          user_id: user_id,
+          tenant_id: tenant_id,
+          eventable: test,
+          body: {
+            'test' => {
+              'id' => test.id } })
+
+        { id: test.id, events: [{ id: event.id, type: event.type }] }
+      end
+    end
+
+    def self.complete(event_id:)
+      transaction do
+        started_event = StartedEvent.find(event_id)
+        test = started_event.eventable
+        test.touch
+
+        event = CompletedEvent.create!(
+          user_id: started_event.user_id,
+          tenant_id: started_event.tenant_id,
+          eventable: test,
+          body: {
+            'test' => {
+              'id' => test.id } })
+
+        { id: test.id, events: [{ id: event.id, type: event.type }] }
+      end
+    end
+
     has_many :events, as: :eventable
   end
 end
