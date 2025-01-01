be906c2a6ff8d3915cc9f9f909f5848de2093740
bedrock-adam <adammikulas@gmail.com>
2025-01-01 21:55:42 +1100

    simplify implementation

--- app/jobs/outboxer_integration/publish_message_job.rb
+++ app/jobs/outboxer_integration/publish_message_job.rb
@@ -4,15 +4,11 @@ module OutboxerIntegration
 
     def perform(args)
       job_class_name = to_job_class_name(messageable_type: args['messageable_type'])
-      job_class_name&.constantize.perform_async('event_id' => args['messageable_id'])
-    rescue NameError
-      # no-op if constant not defined
+      job_class_name&.safe_constantize&.perform_async('event_id' => args['messageable_id'])
     end
 
-    MESSAGEABLE_TYPE_REGEX = /\A(?:(\w+)::)?(\w+)Event\z/
-
     def to_job_class_name(messageable_type:)
-      captures = messageable_type.match(MESSAGEABLE_TYPE_REGEX)&.captures
+      captures = messageable_type.match(/\A(?:(\w+)::)?(\w+)Event\z/)&.captures
       return if captures.nil?
 
       captures[0] ? "#{captures[0]}::#{captures[1]}Job" : "#{captures[1]}Job"
