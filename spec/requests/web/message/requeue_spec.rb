339f09a4709ea1966de1cfbcdad35b96487f9a8f
bedrock-adam <adammikulas@gmail.com>
2025-01-01 22:58:53 +1100

    commit fixed spec

--- spec/requests/web/message/requeue_spec.rb
+++ spec/requests/web/message/requeue_spec.rb
@@ -1,19 +1,28 @@
 require 'spec_helper'
 
-require_relative "../../../lib/outboxer/web"
+require_relative "../../../../lib/outboxer/web"
 
 RSpec.describe 'POST /message/:id/requeue', type: :request do
   include Rack::Test::Methods
+
+  before do
+    header 'Host', 'localhost'
+  end
 
   def app
     Outboxer::Web
   end
 
   it 'requeues a message and redirects' do
-    message = Message.create!(status: 'queued')  # Create test data
+    message = Outboxer::Models::Message.create!(
+      status: 'queued',
+      queued_at: Time.now,
+      messageable_type: 'OutboxerIntegration::TestStartedEvent',
+      messageable_id: '555')
+
     post "/message/#{message.id}/requeue"
     follow_redirect!
     expect(last_response).to be_ok
-    expect(flash[:primary]).to include('was queued')
+    expect(last_request.env['x-rack.flash'][:primary]).to include('was queued')
   end
 end
--- spec/requests/web/publisher/show_spec.rb
+++ spec/requests/web/publisher/show_spec.rb
@@ -1,18 +1,24 @@
 require 'spec_helper'
 
-require_relative "../../../lib/outboxer/web"
+require_relative "../../../../lib/outboxer/web"
 
 RSpec.describe 'GET /publisher/:id', type: :request do
   include Rack::Test::Methods
+
+  before do
+    header 'Host', 'localhost'
+  end
 
   def app
     Outboxer::Web
   end
 
   it 'displays a specific publisher' do
-    publisher = Publisher.create!(name: 'Test Publisher')  # Create test data
+    publisher = Outboxer::Models::Publisher.create!(
+      name: 'Test Publisher',
+      settings: {},
+      metrics: {})
     get "/publisher/#{publisher.id}"
     expect(last_response).to be_ok
-    # Include check for content related to the specific publisher
   end
 end
