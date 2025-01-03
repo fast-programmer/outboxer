eb55c194619d8e2d272ad36d1b6095627a3d03a3
bedrock-adam <adammikulas@gmail.com>
2025-01-03 22:44:49 +1100

    commit web/messages tests (wip)

--- spec/requests/web/messages/delete_all_spec.rb
+++ spec/requests/web/messages/delete_all_spec.rb
@@ -1,16 +1,51 @@
+
 require 'spec_helper'
 
-RSpec.describe 'POST /messages/delete_all', type: :request do
+require_relative '../../../../app/models/application_record'
+require_relative '../../../../app/models/event'
+
+require_relative "../../../../lib/outboxer/web"
+
+RSpec.describe 'GET /messages', type: :request do
   include Rack::Test::Methods
 
   def app
     Outboxer::Web
   end
 
-  it 'deletes all messages older than a specific date' do
-    post '/messages/delete_all', { status: 'archived' }
-    follow_redirect!
-    expect(last_response).to be_ok
-    expect(flash[:primary]).to include('have been deleted')
+  let!(:event) { Event.create!(id: 1, type: 'Event') }
+
+  context 'when no status provided' do
+    before do
+      post '/messages/delete_all'
+    end
+
+    it 'deletes all messages' do
+      expect(Outboxer::Models::Message.count).to eq(0)
+      # expect(last_response.location).to include("/messages?")
+      # follow_redirect!
+      # expect(last_response.body).to include("0 messages have been deleted")
+    end
+
+    it 'redirects to /messages'
+
+    it 'flashes deleted messages count'
+  end
+
+  context 'when queued status provided' do
+    before do
+      post '/messages/delete_all', { status: 'queued' }
+    end
+
+    it 'deletes messages with publishing status' do
+      expect(Outboxer::Models::Message.count).to eq(1)
+      # expect(last_response.location).to include("/messages?status=publishing")
+      # follow_redirect!
+      # expect(last_response.body).to include("1 message has been deleted")
+    end
+
+    it 'redirects to /messages'
+
+    it 'flashes deleted messages count'
   end
 end
--- spec/requests/web/messages/update_per_page_spec.rb
+++ spec/requests/web/messages/update_per_page_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
-require_relative "../../../lib/outboxer/web"
+require_relative '../../../../app/models/application_record'
+require_relative '../../../../app/models/event'
+
+require_relative "../../../../lib/outboxer/web"
 
 RSpec.describe 'POST /messages/update_per_page', type: :request do
   include Rack::Test::Methods
@@ -9,10 +12,24 @@ RSpec.describe 'POST /messages/update_per_page', type: :request do
     Outboxer::Web
   end
 
-  it 'updates number of messages per page and redirects' do
-    post '/messages/update_per_page', { per_page: 50 }
-    follow_redirect!
-    expect(last_response).to be_ok
-    expect(last_request.url).to include('per_page=50')
+  context 'updating pagination settings' do
+    it 'updates number of messages per page and redirects with full parameters' do
+      post '/messages/update_per_page', {
+        status: 'queued',
+        sort: 'created_at',
+        order: 'desc',
+        page: 1,
+        per_page: 10,
+        time_zone: 'UTC'
+      }
+      follow_redirect!
+      expect(last_response).to be_ok
+      expect(last_request.url).to include('per_page=30')
+      expect(last_request.url).to include('status=queued')
+      expect(last_request.url).to include('sort=created_at')
+      expect(last_request.url).to include('order=desc')
+      expect(last_request.url).to include('page=2')
+      expect(last_request.url).to include('time_zone=UTC')
+    end
   end
 end
