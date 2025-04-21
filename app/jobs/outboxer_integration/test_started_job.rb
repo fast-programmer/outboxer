436d3558023b7bf98a8ae06b1059af0767c49391
Adam Mikulasev <adammikulas@gmail.com>
2025-04-21 20:35:02 +1000

    Use positional event job args (#274)

--- app/jobs/outboxer_integration/test_started_job.rb
+++ app/jobs/outboxer_integration/test_started_job.rb
@@ -2,8 +2,8 @@ module OutboxerIntegration
   class TestStartedJob
     include Sidekiq::Job
 
-    def perform(event_id)
-      test_started_event = TestStartedEvent.find(event_id)
+    def perform(event_id, event_type)
+      test_started_event = event_type.safe_constantize.find(event_id)
 
       TestCompletedEvent.create!(body: { "test_id" => test_started_event.body["test_id"] })
     end
