162b4d1ec81cfc978e0d8e3aa27cf5251d1d70f5
bedrock-adam <adammikulas@gmail.com>
2025-06-28 21:24:06 +1000

    point to master

--- quickstart_e2e_tests.sh
+++ quickstart_e2e_tests.sh
@@ -42,7 +42,7 @@ bundle exec rails new . \
 
 bundle install
 bundle exec rails db:create
-echo 'gem "outboxer", git: "https://github.com/fast-programmer/outboxer.git", branch: "refactor/publisher_throughput_and_latency_on_published_at"' \
+echo 'gem "outboxer", git: "https://github.com/fast-programmer/outboxer.git", branch: "master"' \
   >> Gemfile
 bundle install
 bundle exec rails generate outboxer:install
