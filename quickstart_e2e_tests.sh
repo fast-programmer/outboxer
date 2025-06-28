c89e7e8a6fb1df14330db1313c55b9ea937562a7
bedrock-adam <adammikulas@gmail.com>
2025-06-28 19:59:42 +1000

    revert e2e tests

--- quickstart_e2e_tests.sh
+++ quickstart_e2e_tests.sh
@@ -53,47 +53,44 @@ bundle exec ruby -pi -e \
   "sub(/class Event < ApplicationRecord/, \"class Event < ApplicationRecord\\n  after_create { Outboxer::Message.queue(messageable: self) }\")" \
   app/models/event.rb
 
-bundle exec rails runner - <<'RUBY'
-require "outboxer"
+bundle exec rails runner 'Event.create!'
 
-event = Event.create!
-
-env = { "RAILS_ENV" => "development" }
+bundle exec ruby - <<RUBY
 publisher_cmd = File.join(Dir.pwd, "bin", "outboxer_publisher")
-publisher_pid = spawn(env, "ruby", publisher_cmd)
+read_io, write_io = IO.pipe
 
+pid = spawn("ruby", publisher_cmd, out: write_io, err: write_io)
+write_io.close
+
+output = +""
 attempt = 1
-max_attempts = 10
+max_attempts = 20
 delay = 1
 
-was_published = false
+while attempt <= max_attempts
+  begin
+    partial = read_io.read_nonblock(1024)
+    output << partial if partial
+  rescue IO::WaitReadable, EOFError
+  end
 
-was_published = Outboxer::Message
-    .list(status: :published)
-    .fetch(:messages)
-    .any? do |published_message|
-      published_message[:messageable_type] == event.class.name &&
-      published_message[:messageable_id] == event.id.to_s
-    end
+  break if output.include?("published message")
 
-while (attempt <= max_attempts) && !was_published
-    warn "Outboxer message not published yet. Retrying (#{attempt}/#{max_attempts})..."
-    sleep delay
-    attempt += 1
-
-    was_published = Outboxer::Message
-    .list(status: :published)
-    .fetch(:messages)
-    .any? do |published_message|
-      published_message[:messageable_type] == event.class.name &&
-      published_message[:messageable_id] == event.id.to_s
-    end
+  sleep delay
+  attempt += 1
 end
 
-Process.kill("TERM", publisher_pid)
-Process.wait(publisher_pid)
+Process.kill("TERM", pid)
+Process.wait(pid)
+read_io.close
 
-exit(was_published ? 0 : 1)
+if output.include?("published message")
+  puts "Outboxer published message found"
+  exit 0
+else
+  puts "Outboxer published message not found after #{max_attempts} attempts"
+  exit 1
+end
 RUBY
 
 # TARGET_RUBY_VERSION=3.2.2 TARGET_RAILS_VERSION=7.1.5.1 TARGET_DATABASE_ADAPTER=postgresql ./quickstart_e2e_tests.sh
