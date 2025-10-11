ed9afe761680ea85093a50d886da043f11b684a5
bedrock-adam <adammikulas@gmail.com>
2025-10-11 22:02:58 +1100

    fix yard

--- lib/outboxer/publisher.rb
+++ lib/outboxer/publisher.rb
@@ -11,7 +11,7 @@ module Outboxer
 
     # Parses command line arguments to configure the publisher.
     # @param args [Array<String>] The arguments passed via the command line.
-    # @return [Hash] The parsed options
+    # @return [Hash] The parsed options.
     def self.parse_cli_options(args)
       options = {}
 
