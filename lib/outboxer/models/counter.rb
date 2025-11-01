0fc0199031b7e7598b6cee4212d0a26e174f618f
bedrock-adam <adammikulas@gmail.com>
2025-11-01 13:29:15 +1100

    make query sql safe

--- lib/outboxer/models/counter.rb
+++ lib/outboxer/models/counter.rb
@@ -1,32 +1,34 @@
 module Outboxer
   module Models
-    class Counter < ::ActiveRecord::Base
+    class Counter < ActiveRecord::Base
       self.table_name = "outboxer_counters"
 
-      def self.insert_or_increment_by(hostname: Socket.gethostname,
-                                      process_id: Process.pid,
-                                      thread_id: Thread.current.object_id,
-                                      queued_count: 0, publishing_count: 0,
-                                      published_count: 0, failed_count: 0,
-                                      time: Time.now.utc)
-        adapter = ActiveRecord::Base.connection.adapter_name.downcase
+      def self.insert_or_increment_by(
+        hostname: Socket.gethostname,
+        process_id: Process.pid,
+        thread_id: Thread.current.object_id,
+        queued_count: 0,
+        publishing_count: 0,
+        published_count: 0,
+        failed_count: 0,
+        time: Time.now.utc
+      )
         now = time.utc
 
-        sql = if adapter.include?("postgres")
+        sql = if connection.adapter_name.downcase.include?("postgres")
                 <<~SQL
                   INSERT INTO #{table_name}
                     (hostname, process_id, thread_id,
                      queued_count, publishing_count, published_count, failed_count,
                      created_at, updated_at)
-                  VALUES (?, ?, ?, #{queued_count}, #{publishing_count},
-                          #{published_count}, #{failed_count}, ?, ?)
+                  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
                   ON CONFLICT (hostname, process_id, thread_id)
                   DO UPDATE SET
-                    queued_count     = #{table_name}.queued_count     + #{queued_count},
-                    publishing_count = #{table_name}.publishing_count + #{publishing_count},
-                    published_count  = #{table_name}.published_count  + #{published_count},
-                    failed_count     = #{table_name}.failed_count     + #{failed_count},
-                    updated_at       = EXCLUDED.updated_at
+                    queued_count     = #{table_name}.queued_count     + ?,
+                    publishing_count = #{table_name}.publishing_count + ?,
+                    published_count  = #{table_name}.published_count  + ?,
+                    failed_count     = #{table_name}.failed_count     + ?,
+                    updated_at       = ?
                 SQL
               else
                 <<~SQL
@@ -34,21 +36,24 @@ module Outboxer
                     (hostname, process_id, thread_id,
                      queued_count, publishing_count, published_count, failed_count,
                      created_at, updated_at)
-                  VALUES (?, ?, ?, #{queued_count}, #{publishing_count},
-                          #{published_count}, #{failed_count}, ?, ?)
+                  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
                   ON DUPLICATE KEY UPDATE
-                    queued_count     = queued_count     + #{queued_count},
-                    publishing_count = publishing_count + #{publishing_count},
-                    published_count  = published_count  + #{published_count},
-                    failed_count     = failed_count     + #{failed_count},
-                    updated_at       = VALUES(updated_at)
+                    queued_count     = queued_count     + ?,
+                    publishing_count = publishing_count + ?,
+                    published_count  = published_count  + ?,
+                    failed_count     = failed_count     + ?,
+                    updated_at       = ?
                 SQL
               end
 
-        ActiveRecord::Base.connection.exec_query(
-          ActiveRecord::Base.sanitize_sql_array(
-            [sql, hostname, process_id, thread_id, now, now]
-          )
+        connection.exec_query(
+          sanitize_sql_array([
+            sql,
+            hostname, process_id, thread_id,
+            queued_count, publishing_count, published_count, failed_count,
+            now, now,
+            queued_count, publishing_count, published_count, failed_count, now
+          ])
         )
 
         nil
