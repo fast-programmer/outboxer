c2720b3fa82e8780a4771e65d9f8a31ac991bb60
Adam Mikulasev <adammikulas@gmail.com>
2025-12-13 02:59:34 +1100

    publish must receive block (#394)

--- quickstart_e2e_tests.sh
+++ quickstart_e2e_tests.sh
@@ -42,7 +42,7 @@ bundle exec rails new . \
 
 bundle install
 bundle exec rails db:create
-echo 'gem "outboxer", git: "https://github.com/fast-programmer/outboxer.git", branch: "optimisation/increase_throughput"' \
+echo 'gem "outboxer", git: "https://github.com/fast-programmer/outboxer.git", branch: "'"${GITHUB_HEAD_REF}"'"' \
   >> Gemfile
 bundle install
 bundle exec rails generate outboxer:install
